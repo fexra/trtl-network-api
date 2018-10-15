4481e13607c90c493d19fb3c3b22a2fc23acf628
fexra <fexra@protonmail.com>
2018-10-15 03:36:11 +0000

    add more source nodes

--- config/nodes.js
+++ config/nodes.js
@@ -6,11 +6,16 @@
 module.exports = [ 
     'http://public.turtlenode.io:11898',
     'http://asia.turtlenode.io:11898',
-    'http://japakar.turtle.com:11898',
+    'http://145.131.30.157:11898',
     'http://sewer.turtlecoin.club:11898',
     'http://node.turtlecoin.club:11898',
     'http://val.turtlenode.online:11898',
     'http://public.turtlenode.net:11898',
     'http://europe.turtlenode.io:11898',
-    'http://turtle.land:11898'   
+    'http://turtle.land:11898',
+    'http://142.44.242.106:11898',
+    'http://47.52.165.1:11898',
+    'http://5.189.155.86:11898',
+    'http://58.185.155.109:11898',
+    'http://151.80.44.33:11898'
 ]
